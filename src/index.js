101b3c85b12157e9e55264780fab2397cf6bd266
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-25 07:05:25 +0200

    fix

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './basic.css';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
