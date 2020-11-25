87965734f617fec4a5c95d2a557122136fbc8851
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-25 07:53:29 +0200

    fix

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead className={styles.tableHead}>
+        <tr>
+          <th className={styles.title}>Type</th>
+          <th className={styles.title}>Amount</th>
+          <th className={styles.title}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id} className={styles.text}>
+            <td className={styles.item}>{type}</td>
+            <td className={styles.item}>{amount}</td>
+            <td className={styles.item}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+TransactionHistory.propTypes = {
+  items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+};
+export default TransactionHistory;
